da67d8a42ca2f683c48cd1d08395c0871e327a52
Alex P. <alkoclick@gmail.com>
2017-10-23 20:38:56 +0200

    Add problem description and javadoc

--- src/main/java/interviews/FirstNonRepeatedCharacter.java
+++ src/main/java/interviews/FirstNonRepeatedCharacter.java
@@ -1,7 +1,19 @@
 package interviews;
 
+/**
+ * Problem description:
+ * http://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
+ */
 public class FirstNonRepeatedCharacter {
 
+	/**
+	 * Given a string, returns the first character x that does not appear more
+	 * than once. For example, for input 'test' it should return 'e'
+	 * 
+	 * @param input
+	 *            A string which to check for repeating characters
+	 * @return The first non repeating character found, or Character.MIN_VALUE
+	 */
 	public static char of(String input) {
 		return (char) input.chars().
 		// Keep only elements apearing exactly once
